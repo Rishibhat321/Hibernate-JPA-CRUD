cb2f217db8f874e1693cb79509314b4e24e92df7
Rishib#321 <rishitab2209@gmail.com>
2024-12-22 18:56:32 +0530

    Updated the main application

--- 01-CrudDemo-student/src/main/java/com/HibernateJpa/CrudDemo/CrudDemoApplication.java
+++ 01-CrudDemo-student/src/main/java/com/HibernateJpa/CrudDemo/CrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.HibernateJpa.CrudDemo;
 
+import com.HibernateJpa.CrudDemo.dao.StudentDAO;
+import com.HibernateJpa.CrudDemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +14,35 @@ public class CrudDemoApplication {
 		SpringApplication.run(CrudDemoApplication.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	public CommandLineRunner commandLineRunner(String[] args) {
 		return runner -> {
 			System.out.println("Hello World");
 		};
+	}  */
+
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ... ");
+		Student tempStudent = new Student("Sahil" , "Arora" , "sahil@gmail.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display the id of the saved student
+		System.out.println("Saved student Generated id: " + tempStudent.getId());
+
 	}
 
 }
