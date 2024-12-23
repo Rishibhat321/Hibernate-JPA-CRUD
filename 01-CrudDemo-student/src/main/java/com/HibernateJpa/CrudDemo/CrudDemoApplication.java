75fa3c69403030fa103b03bc09a6393368912e0d
Rishib#321 <rishitab2209@gmail.com>
2024-12-23 18:29:07 +0530

    Updated the main application

--- 01-CrudDemo-student/src/main/java/com/HibernateJpa/CrudDemo/CrudDemoApplication.java
+++ 01-CrudDemo-student/src/main/java/com/HibernateJpa/CrudDemo/CrudDemoApplication.java
@@ -26,8 +26,52 @@ public class CrudDemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
 
-			createStudent(studentDAO);
+	//		createStudent(studentDAO);
+
+	//		createMultipeStudents(studentDAO);
+
+			readStudent(studentDAO);
 		};
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		// create a student object
+		System.out.println("Creating a new student object ... ");
+		Student tempStudent = new Student("John", "Bhat", "johnb@gmail.com");
+
+		// save the student
+		System.out.println("Saving the student");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student Generated id: " + theId);
+
+		// retrieve student based on the id: primary key
+		System.out.println("Retreiving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// display the student
+		System.out.println("Found student: " + myStudent);
+
+	}
+
+	private void createMultipeStudents(StudentDAO studentDAO) {
+
+		// create multiple students
+		System.out.println("Creating new student object ... ");
+		Student tempStudent1 = new Student("John" , "Doe" , "john@gmail.com");
+		Student tempStudent2 = new Student("Mary" , "Jane" , "mary@gmail.com");
+		Student tempStudent3 = new Student("Rishi" , "Dubey" , "rishi@gmail.com");
+
+
+		// save the student objects
+		System.out.println("Saving the students...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
