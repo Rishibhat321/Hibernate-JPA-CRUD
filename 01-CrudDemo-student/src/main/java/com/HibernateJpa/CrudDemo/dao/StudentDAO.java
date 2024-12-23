e3cc28c0ff3b7e8117cda6fcd2332df9849b2d7e
Rishib#321 <rishitab2209@gmail.com>
2024-12-23 20:55:38 +0530

    Created methods for CRUD Operations

--- 01-CrudDemo-student/src/main/java/com/HibernateJpa/CrudDemo/dao/StudentDAO.java
+++ 01-CrudDemo-student/src/main/java/com/HibernateJpa/CrudDemo/dao/StudentDAO.java
@@ -2,9 +2,23 @@ package com.HibernateJpa.CrudDemo.dao;
 
 import com.HibernateJpa.CrudDemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    // delete all the students from the db
+    int deleteAll();
+
 }
